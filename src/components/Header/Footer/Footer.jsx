d78679a414de8344483fa0b828b46d20b48ef2fc
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-18 16:21:42 -0400

    Finished Footer component

---
+++ src/components/Header/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="footer-container">
+        <p>Made with &#9829; by mimo</p>
+        <button className="contact-us-button">Contact Us</button>
+    </div>
+  )
+}
+
+export default Footer
