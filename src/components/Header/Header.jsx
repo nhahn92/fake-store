5007cd212ddc49cb7d76917b8fbfdcb4dbabcc40
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-18 15:59:13 -0400

    Created the Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+import { HiOutlineShoppingCart } from 'react-icons/hi'
+
+function Header() {
+  return (
+    <div className="header-container">
+        <h1>Fake Store</h1>
+        <div className="shopping-cart-container">
+            <HiOutlineShoppingCart className="shopping-cart-icon" />
+            <div className="shopping-cart-number-container">
+                <p>1</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
