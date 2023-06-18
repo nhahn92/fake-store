837efa5720014b962b25c215815b6dc1b6a86fcc
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-18 18:05:56 -0400

    Imported Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Header/Footer/Footer'
+import Homepage from './pages/Homepage'
 
 function App() {
   return (
     <div>
       <Header />
+      <Homepage />
       <Footer />
     </div>
   )
