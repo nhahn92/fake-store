a16697358193d43b2094bc87d1d68d6644f0a2cf
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-18 16:22:10 -0400

    Added finished Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Header/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   )
 }
