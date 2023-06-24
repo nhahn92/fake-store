fc023868a1429d323cb9f496cc939bd983189aee
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-24 19:10:16 -0400

    Imported pages for routing; set up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
-import Footer from './components/Header/Footer/Footer'
-import Homepage from './pages/Homepage'
+import Footer from './components/Footer/Footer'
+import Homepage from './pages/Homepage/Homepage'
+import ContactUs from './pages/ContactUs/ContactUs'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Homepage />
+
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/contactus' element={<ContactUs />} />
+        <Route path='/details/:productId' element={<ProductDetails />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </BrowserRouter>
   )
 }
 
