d469042cb4e341a747404a5eb780c6c35d5457a5
Nicole Hahn <nicolehahn92@gmail.com>
2023-06-27 10:56:32 -0400

    Added ThemeContextProvider for Dark Mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import Footer from './components/Footer/Footer'
 import Homepage from './pages/Homepage/Homepage'
 import ContactUs from './pages/ContactUs/ContactUs'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
+import ThemeContextProvider from './contexts/ThemeContext'
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      <ThemeContextProvider>
+        <Header />
 
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/contactus' element={<ContactUs />} />
-        <Route path='/details/:productId' element={<ProductDetails />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/contactus' element={<ContactUs />} />
+          <Route path='/details/:productId' element={<ProductDetails />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
+      </ThemeContextProvider>
     </BrowserRouter>
   )
 }
