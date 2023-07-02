54bc9020e7f296826ce9d4ad12134c6d57c58491
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-02 14:18:25 -0400

    Added modal to "Contact Us" button

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
 import './Footer.css'
+import Modal from 'react-modal';
+
+// Modal from react-modal
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  }
+};
+
+// Make sure to bind Modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement(document.getElementById('root'));
 
 function Footer() {
+  // Create state to control Modal
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
     <div className="footer-container">
         <p>Made with &#9829; by mimo</p>
-        <a href="#" className="contact-us-button">Contact Us</a>
+        <a href="#" className="contact-us-button" onClick={() => setIsOpen(true)}>Contact Us</a>
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={() => setIsOpen(false)}
+          style={customStyles}
+          contentLabel="Contact Us Modal"
+        >
+        <div className="modal-header">
+          <h2>Contact Us</h2>
+          <button className="modal-close-button" onClick={() => setIsOpen(false)}>x</button>
+        </div>
+        <form>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" rows="4"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </Modal>
     </div>
   )
 }
