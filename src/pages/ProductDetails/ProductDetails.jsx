d35f80e601b3f61283728627746ca19ee0c3a794
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-03 22:12:19 -0400

    Added a back arrow to return to homepage

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,9 @@
-import { React, useState, useEffect, useContext } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect, useContext } from 'react'
+import { useParams, Link } from 'react-router-dom'
 import './ProductDetails.css'
 import axios from 'axios'
 import { ThemeContext } from '../../contexts/ThemeContext'
+import { BsArrowReturnLeft } from 'react-icons/bs'
 
 function ProductDetails() {
     // Change to use global state
@@ -35,6 +36,7 @@ function ProductDetails() {
     <div className={darkMode?"product-container product-container-dark" : "product-container"}>
         <img src={product?.image} />
         <div className="details-container">
+            <Link to="/"><BsArrowReturnLeft className="back-arrow-icon" /></Link>
             <h2>{product?.title}</h2>
             <h2>{product?.price} €</h2>
             <h3>Description</h3>
