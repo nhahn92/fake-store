0e31a2bdc88ce150141ee0edf79f89cb4c610e71
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-03 22:13:04 -0400

    Worked on Checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import './Checkout.css'
+import { ThemeContext } from '../../contexts/ThemeContext'
 
 function Checkout() {
+  // Change to use global state
+  // Note: {} not []
+  const {darkMode, setDarkMode} = useContext(ThemeContext)
+
   return (
-    <div>
-        Checkout
+    <div className={darkMode?"checkout-container checkout-container-dark" : "checkout-container"}>
+      <div className="checkout-header-container">
+        <p className="checkout-item-header">Item</p>
+        <p className="checkout-price-header">Price</p>
+        <p className="checkout-quantity-header">Quantity</p>
+        <p className="checkout-remove-header">Remove</p>
+      </div>
+      <div className="checkout-contents-container">
+
+      </div>
+      <div className="checkout-footer-container">
+        <p>Total</p>
+        <button>Checkout</button>
+      </div>
     </div>
   )
 }
