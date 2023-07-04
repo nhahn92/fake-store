a6a1ed69801b6262a30ce201a9fafee09c226218
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-03 22:13:36 -0400

    Finished heart icon functionality on products

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,17 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import './ProductCard.css'
-import { BiHeartCircle } from "react-icons/bi"
+import { BsHeartFill, BsHeart } from "react-icons/bs"
 import { ThemeContext } from '../../contexts/ThemeContext'
 import { Link } from 'react-router-dom'
+import { CartContext } from '../../contexts/CartContext'
 
 function ProductCard({product}) {
   // Change to use global state
   // Note: {} not []
   const {darkMode, setDarkMode} = useContext(ThemeContext)
 
+  const {cart, addProduct, removeProduct} = useContext(CartContext)
+
+  // Create variable to control heart icons
+  // Change to state
+  const [isInCart, setIsInCart] = React.useState(false)
+
+  // Need to check if this product is in the cart
+  // any time cartContents changes
+  useEffect (
+    () => {
+      // Is this product in the cart?
+      setIsInCart(cart.find(item => item.id == product.id))
+    }
+  ), [cart] // Runs when cartContents changes
+
   return (
     <div className={darkMode?"product-card product-card-dark" : "product-card"}>
-        <BiHeartCircle className="heart-icon" />
+        {
+          isInCart?
+          <BsHeartFill className="heart-icon" onClick={() => removeProduct(product?.id)} />
+          :
+          <BsHeart className="heart-outline-icon" onClick={() => addProduct(product)} />
+        }
         <div className={darkMode?"product-image-container product-image-container-dark" : "product-image-container"}>
           <img src={product?.image} />
         </div>
