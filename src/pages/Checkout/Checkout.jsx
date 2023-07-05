bf5683e9cba4c5090363e28db5cf08da986064cf
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-04 21:08:43 -0400

    Mostly completed but still not done

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,26 +1,48 @@
 import React, { useState, useEffect, useContext } from 'react'
 import './Checkout.css'
 import { ThemeContext } from '../../contexts/ThemeContext'
+import { CartContext } from '../../contexts/CartContext'
+import { FaTrashAlt } from 'react-icons/fa'
 
 function Checkout() {
   // Change to use global state
   // Note: {} not []
-  const {darkMode, setDarkMode} = useContext(ThemeContext)
+  const {darkMode} = useContext(ThemeContext)
+
+  const {cart, removeProduct} = useContext(CartContext)
 
   return (
     <div className={darkMode?"checkout-container checkout-container-dark" : "checkout-container"}>
       <div className="checkout-header-container">
-        <p className="checkout-item-header">Item</p>
-        <p className="checkout-price-header">Price</p>
-        <p className="checkout-quantity-header">Quantity</p>
-        <p className="checkout-remove-header">Remove</p>
+        <p className="checkout-contents-left">Item</p>
+        <div className="checkout-contents-right">
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Remove</p>
+        </div>
       </div>
-      <div className="checkout-contents-container">
-
-      </div>
+      {
+        cart.map(function(item) {
+          return (
+            <>
+              <div className="checkout-contents-container">
+                <div className="checkout-contents-left">
+                  <img src={item.image} />
+                  <p className="checkout-product-title">{item.title}</p>
+                </div>
+                <div className="checkout-contents-right">
+                  <p>{item.price} €</p>
+                  <p>1</p>
+                  <FaTrashAlt className="checkout-trashcan-icon" onClick={() => removeProduct(item?.id)} />
+                </div>
+              </div>
+            </>
+          )
+        })
+      }
       <div className="checkout-footer-container">
         <p>Total</p>
-        <button>Checkout</button>
+        <button className="checkout-button">Checkout</button>
       </div>
     </div>
   )
