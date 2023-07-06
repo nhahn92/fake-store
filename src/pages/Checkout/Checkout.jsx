c5a233b7ab37ab2159310185c39433ca6243ecbc
Nicole Hahn <nicolehahn92@gmail.com>
2023-07-05 20:06:21 -0400

    Finished the layout; added total and dark mode

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -3,6 +3,7 @@ import './Checkout.css'
 import { ThemeContext } from '../../contexts/ThemeContext'
 import { CartContext } from '../../contexts/CartContext'
 import { FaTrashAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Checkout() {
   // Change to use global state
@@ -10,10 +11,17 @@ function Checkout() {
   const {darkMode} = useContext(ThemeContext)
 
   const {cart, removeProduct} = useContext(CartContext)
+  console.log(cart)
+
+  // Find the total price of cart contents
+  let total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].price;
+  }
 
   return (
     <div className={darkMode?"checkout-container checkout-container-dark" : "checkout-container"}>
-      <div className="checkout-header-container">
+      <div className={darkMode?"checkout-header-container checkout-dark" : "checkout-header-container"}>
         <p className="checkout-contents-left">Item</p>
         <div className="checkout-contents-right">
           <p>Price</p>
@@ -25,14 +33,14 @@ function Checkout() {
         cart.map(function(item) {
           return (
             <>
-              <div className="checkout-contents-container">
+              <div className={darkMode?"checkout-contents-container checkout-dark" : "checkout-contents-container"}>
                 <div className="checkout-contents-left">
-                  <img src={item.image} />
-                  <p className="checkout-product-title">{item.title}</p>
+                  <img src={item?.image} />
+                  <Link to={`/details/${item.id}`}><p className="checkout-product-title">{item?.title}</p></Link>
                 </div>
                 <div className="checkout-contents-right">
-                  <p>{item.price} €</p>
-                  <p>1</p>
+                  <p className="checkout-product-price">{item?.price} €</p>
+                  <p className="checkout-product-quantity">1</p>
                   <FaTrashAlt className="checkout-trashcan-icon" onClick={() => removeProduct(item?.id)} />
                 </div>
               </div>
@@ -40,9 +48,14 @@ function Checkout() {
           )
         })
       }
-      <div className="checkout-footer-container">
-        <p>Total</p>
-        <button className="checkout-button">Checkout</button>
+      <div className={darkMode?"checkout-footer-container checkout-dark" : "checkout-footer-container"}>
+        <div className="checkout-footer-contents-container">
+          <div>
+            <p>Total</p>
+            <p>{total.toFixed(2)} €</p>
+          </div>
+          <button className="checkout-button">Checkout</button>
+        </div>
       </div>
     </div>
   )
